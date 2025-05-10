aec4be7ede8b160776126c48dbf0b024f85ca769
PandaGuerrier <76222146+PandaGuerrier@users.noreply.github.com>
2025-05-10 20:50:30 +0200

    feat: first commit

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,115 +1,47 @@
-import Image from "next/image";
-import { Geist, Geist_Mono } from "next/font/google";
-
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
+import { AnimatedFilmChoice } from '@/ui/animated-film-choice'
+import { Toaster } from 'sonner'
 
 export default function Home() {
+  const testimonials = [
+    {
+      quote: "Harry Potter est un film fantastique qui nous plonge dans un monde magique rempli d'aventures et de mystères.",
+      name: "Harry Potter",
+      tags: ["Science-Fiction", "Comedie", "Action"],
+      image: "https://www.closeupshop.fr/media/oart_0/oart_h/oart_96658/1070074_G880485_07.JPG",
+    },
+    {
+      quote: "Sonic, Knuckles et Tails sont à nouveau réunis face à un puissant nouvel adversaire, Shadow, un mystérieux vilain doté de pouvoirs comme ils n'en ont encore jamais vu. Leurs habiletés étant toutes surclassées, l'Équipe Sonic doit tenter une alliance improbable dans l'espoir d'arrêter Shadow et de protéger la planète.",
+      name: "Sonic 3, le film",
+      tags: ["Action",
+        "Science-fiction",
+        "Comédie",
+        "Famille"],
+      image: "https://m.media-amazon.com/images/M/MV5BNGRhYjY5MWItNGI0ZS00OTVkLTkyYWYtMzYwYjU1NTk3YWIyXkEyXkFqcGc@._V1_FMjpg_UX1000_.jpg",
+    },
+    {
+      quote: "Harry Potter est un film fantastique qui nous plonge dans un monde magique rempli d'aventures et de mystères.",
+      name: "Captain America : Brave New World",
+      tags: ["Science-Fiction", "Thriller", "Action"],
+      image: "https://fr.web.img2.acsta.net/img/92/55/9255d9aeb17004f6a622cc47e573777e.jpg",
+    },
+    {
+      quote: "Lors d'un safari avec son père Nikolai, un oligarque russe dont la cruauté a jadis poussé sa mère au suicide, Sergei Kravinoff est attaqué par un lion, qui vient très près de le tuer. La potion miraculeuse de Calypso, une prêtresse vaudou, permet non seulement au garçon de survivre ; elle lui confère aussi une force surhumaine et un nouvel objectif : protéger la nature des braconniers et des criminels de toutes sortes. Élevé au titre de \"chasseur\", ses faits d’armes attirent rapidement l’attention d’un assassin redoutable, dont les pouvoirs sont comparables aux siens : le Rhino.",
+      name: "Kraven the Hunter",
+      tags: ["Thriller", "Aventure", "Action"],
+      image: "https://fr.web.img3.acsta.net/img/bf/75/bf759d8af9d0eb3b279a802b179ae4cb.jpg",
+    },
+    {
+      quote: "Le Cardinal Lawrence est chargé de gérer ce processus confidentiel après la mort inattendue du Pape bien-aimé. Lorsque tous les dirigeants les plus puissants de l’Église catholique sont réunis et enfermés dans le Vatican, Lawrence se retrouve au centre d’une conspiration et découvre un secret qui pourrait changer à jamais l’institution religieuse.\n" +
+          "\n",
+      name: "Conclave",
+      tags: ["Thriller", "Drame", "Mystère"],
+      image: "https://m.media-amazon.com/images/S/pv-target-images/90675cc6356f893ec6f202b0e402319129e456479db0dce57fbb7da91469fac4.jpg",
+    },
+  ];
   return (
-    <div
-      className={`${geistSans.className} ${geistMono.className} grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]`}
-    >
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm/6 text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2 tracking-[-.01em]">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-[family-name:var(--font-geist-mono)] font-semibold">
-              src/pages/index.tsx
-            </code>
-            .
-          </li>
-          <li className="tracking-[-.01em]">
-            Save and see your changes instantly.
-          </li>
-        </ol>
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
+      <>
+        <Toaster />
+        <AnimatedFilmChoice films={testimonials} />
+      </>
   );
 }
--- src/ui/animated-film-choice.tsx
+++ src/ui/animated-film-choice.tsx
@@ -1,43 +1,47 @@
 "use client";
 
-import { IconArrowLeft, IconArrowRight, IconBellX, IconHeart, IconSkewX, IconX } from '@tabler/icons-react'
+import { IconHeart, IconX } from '@tabler/icons-react'
 import { motion, AnimatePresence } from "motion/react";
 
 import { useEffect, useState } from "react";
+import { toast } from 'sonner';
 
-type Testimonial = {
+type Film = {
   quote: string;
   name: string;
-  designation: string;
-  src: string;
+  tags: string[];
+  image: string;
 };
-export const AnimatedTestimonials = ({
-                                       testimonials,
-                                       autoplay = false,
-                                     }: {
-  testimonials: Testimonial[];
-  autoplay?: boolean;
-}) => {
+export const AnimatedFilmChoice = ({ films }: { films: Film[] }) => {
   const [active, setActive] = useState(0);
+  const [seen, setSeen] = useState([0]);
 
-  const handleNext = () => {
-    setActive((prev) => (prev + 1) % testimonials.length);
+  const handleLike = () => {
+    setActive((prev) => (prev + 1) % films.length);
+    setSeen((prev) => {
+      const next = (prev[prev.length - 1] + 1) % films.length;
+      return [...prev, next];
+    });
+    toast.success("Film ajouté à votre liste de favoris !", {
+      richColors: true,
+    });
   };
 
-  const handlePrev = () => {
-    setActive((prev) => (prev - 1 + testimonials.length) % testimonials.length);
+  const handleDislike = () => {
+    setActive((prev) => (prev + 1) % films.length);
+    setSeen((prev) => {
+      const next = (prev[prev.length - 1] + 1) % films.length;
+      return [...prev, next];
+    });
+
+    toast.error("Film retiré de votre liste de favoris !", {
+      richColors: true,
+    });
   };
 
   const isActive = (index: number) => {
     return index === active;
   };
-
-  useEffect(() => {
-    if (autoplay) {
-      const interval = setInterval(handleNext, 5000);
-      return () => clearInterval(interval);
-    }
-  }, [autoplay]);
 
   const randomRotateY = () => {
     return Math.floor(Math.random() * 21) - 10;
@@ -48,9 +52,9 @@ export const AnimatedTestimonials = ({
           <div>
             <div className="relative h-80 w-full">
               <AnimatePresence>
-                {testimonials.map((testimonial, index) => (
+                {films.map((film, index) => (
                     <motion.div
-                        key={testimonial.src}
+                        key={film.name}
                         initial={{
                           opacity: 0,
                           scale: 0.9,
@@ -64,7 +68,7 @@ export const AnimatedTestimonials = ({
                           rotate: isActive(index) ? 0 : randomRotateY(),
                           zIndex: isActive(index)
                               ? 40
-                              : testimonials.length + 2 - index,
+                              : films.length + 2 - index,
                           y: isActive(index) ? [0, -80, 0] : 0,
                         }}
                         exit={{
@@ -77,22 +81,21 @@ export const AnimatedTestimonials = ({
                           duration: 0.4,
                           ease: "easeInOut",
                         }}
-                        className="absolute inset-0 origin-bottom"
+                        className="absolute inset-0 origin-bottom flex items-center justify-center"
                     >
                       <img
-                          src={testimonial.src}
-                          alt={testimonial.name}
-                          width={500}
-                          height={500}
+                          src={film.image}
+                          alt={film.name}
+                          width={250}
                           draggable={false}
-                          className="h-full w-full rounded-3xl object-cover object-center"
+                          className="rounded-3xl object-cover object-center"
                       />
                     </motion.div>
                 ))}
               </AnimatePresence>
             </div>
           </div>
-          <div className="flex flex-col justify-between py-4">
+          <div className="flex flex-col justify-between space-y-4 py-4">
             <motion.div
                 key={active}
                 initial={{
@@ -112,14 +115,21 @@ export const AnimatedTestimonials = ({
                   ease: "easeInOut",
                 }}
             >
-              <h3 className="text-2xl font-bold text-black dark:text-white">
-                {testimonials[active].name}
+              <h3 className="text-2xl font-bold text-white text-center">
+                {films[active].name}
               </h3>
-              <p className="text-sm text-gray-500 dark:text-neutral-500">
-                {testimonials[active].designation}
-              </p>
-              <motion.p className="mt-8 text-lg text-gray-500 dark:text-neutral-300">
-                {testimonials[active].quote.split(" ").map((word, index) => (
+              <div className={"flex justify-center"}>
+                {films[active].tags.map((tag, index) => (
+                    <span
+                        key={index}
+                        className="mr-2 inline-block rounded-full bg-gray-200 px-3 py-1 text-sm font-semibold text-gray-700 dark:bg-neutral-700 dark:text-neutral-300"
+                    >
+                      {tag}
+                    </span>
+                ))}
+              </div>
+              <motion.p className="mt-8 text-lg text-neutral-300 text-center">
+                {films[active].quote.split(" ").map((word, index) => (
                     <motion.span
                         key={index}
                         initial={{
@@ -146,16 +156,25 @@ export const AnimatedTestimonials = ({
             </motion.div>
             <div className="flex w-full justify-center gap-4 pt-12 md:pt-0">
               <button
-                  onClick={handleNext}
+                  onClick={handleDislike}
                   className="group/button flex items-center justify-center rounded-full p-4 bg-red-400"
               >
-                <IconX className="h-14 w-14 text-white transition-transform duration-300 group-hover/button:rotate-12" />
+                <IconX className="h-14 w-14 text-white transition-transform duration-300 group-hover/button:rotate-12"/>
               </button>
               <button
-                  onClick={handleNext}
+                  onClick={handleLike}
                   className="group/button flex items-center justify-center rounded-full p-4 bg-green-400"
               >
-                <IconHeart className="h-14 w-14 text-white transition-transform duration-300 group-hover/button:-rotate-24" />
+                <IconHeart
+                    className="h-14 w-14 text-white transition-transform duration-300 group-hover/button:-rotate-24"/>
+              </button>
+            </div>
+            <div className={"flex justify-center"}>
+              <button
+                  onClick={handleLike}
+                  className="group/button flex items-center w-1/2 justify-center rounded-full p-4 bg-neutral-600"
+              >
+                Voir le film
               </button>
             </div>
           </div>
